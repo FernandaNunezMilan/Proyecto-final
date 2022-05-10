22015f44cf71e145422329350d077a7de6fb757d
FernandaNunezMilan <maria.nunez7585@alumnos.udg.mx>
2022-05-09 21:24:52 -0500

    Updated

--- js/main.js
+++ js/main.js
@@ -13,6 +13,100 @@ const tutoriales = document.querySelectorAll('.tutorial');
 const flechaIzquierda = document.getElementById('flecha-izquierda');
 const flechaDerecha = document.getElementById('flecha-derecha');
 
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), 
+    {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 700,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista2'), 
+    {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores2',
+		arrows: {
+			prev: '.carousel__anterior2',
+			next: '.carousel__siguiente2'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 700,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista3'), 
+    {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores3',
+		arrows: {
+			prev: '.carousel__anterior3',
+			next: '.carousel__siguiente3'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 700,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
+
 // ------- --------- Event listener de las flechas ------- ---------- //
 
 flechaDerecha.addEventListener('click', () => {
